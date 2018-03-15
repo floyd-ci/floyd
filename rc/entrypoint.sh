a2a05da40daf41209fa15e251cf09da17f116210
Regina <regina@mailbox.org>
2018-03-15 11:35:22 +0100

    bash is not required

--- rc/entrypoint.sh
+++ rc/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Execute system setup hook
 if [ -f /systemsetup.sh ]
@@ -9,10 +9,10 @@ fi
 # If we are running docker natively, we want to create a user in the container
 # with the same UID and GID as the user on the host machine, so that any files
 # created are owned by that user. Without this they are all owned by root.
-if [[ -n $BUILDER_UID ]] && [[ -n $BUILDER_GID ]]; then
+if test -n "$BUILDER_UID" && test -n "$BUILDER_GID"
+then
     groupadd -o -g $BUILDER_GID $BUILDER_GROUP 2> /dev/null
     useradd -o -m -g $BUILDER_GID -u $BUILDER_UID $BUILDER_USER 2> /dev/null
-    shopt -s dotglob
 
     # Make sure the home directory is owned by the specified user/group.
     chown -R $BUILDER_UID:$BUILDER_GID $HOME
--- rc/entrypoint2.sh
+++ rc/entrypoint2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Execute user setup hook
 if [ -f /usersetup.sh ]
